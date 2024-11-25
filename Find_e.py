e6ba0e531fba6d385bb3f22068e4d6c91e0a8ca6
OLIVIER PORTAL <olivier.portal@laplateforme.io>
2024-11-25 20:02:07 +0100

    Find lettre e in text

---
+++ Find_e.py
@@ -0,0 +1,18 @@
+
+my_string = "Hello, world!" 
+
+char_to_check = 'e' 
+ 
+if char_to_check in my_string: 
+    print(f"La lettre '{char_to_check}' est dnas le texte.") 
+else: 
+    print(f"La lettre '{char_to_check}' n'est pas dans le texte") 
+    
+# test 2
+
+char_to_check2 = 'a'
+
+if char_to_check2 in my_string: 
+    print(f"La lettre '{char_to_check2}' est dnas le texte.") 
+else: 
+    print(f"La lettre '{char_to_check2}' n'est pas dans le texte") 
