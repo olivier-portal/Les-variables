9fbd4a3c4ea47a86691fc0de48e5c8688c71a50d
OLIVIER PORTAL <olivier.portal@laplateforme.io>
2024-11-25 13:29:42 +0100

    add inventory excercie

---
+++ inventory.py
@@ -0,0 +1,11 @@
+# name:string ='logiciel comptabilité'
+# price:float = 20.50
+# quantity:int = 4
+
+product_detail = {
+    "name": "Samsung galaxy",
+    "price": 650,
+    "quantity": 52,
+}
+
+print(product_detail)
