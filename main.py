30cbcdee15122f618947195cb0da382e960d73b1
OLIVIER PORTAL <olivier.portal@laplateforme.io>
2024-11-25 11:46:31 +0100

    JOB09 start

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ lower = list(string.ascii_lowercase)
 print(lower)
 print(lower[::-1])
 
+upper = list(string.ascii_uppercase)
+upper.reverse()
+print('reverse list of upper', upper)
+
 phrase = 'Je suis un String'
 ma_string: string = phrase
 print(ma_string)
@@ -21,3 +25,11 @@ num2 = 14
 
 multiply_numbers = (num1 * num2)
 print(multiply_numbers)
+
+name:string ='logiciel comptabilité'
+price:float = 20.50
+quantity:int = 4
+
+print(f'Informations du produit: {'Nom :', name} {'prix :', price, '€'} {'Quantité :', quantity}')
+print(f'Informations du produit ====> Nom : {name}, prix : {price}€, Quantité : {quantity}')
+
