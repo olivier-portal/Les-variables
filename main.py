2b3c6b2c8a973eae8d1356594db61b4b92466c3c
OLIVIER PORTAL <olivier.portal@laplateforme.io>
2024-11-25 10:55:33 +0100

    JOB07 and JOB08

--- main.py
+++ main.py
@@ -9,4 +9,15 @@ phrase = 'Je suis un String'
 ma_string: string = phrase
 print(ma_string)
 
+num1 = 40
+num2 = 2
 
+add_numbers = (num1 + num2)
+print(add_numbers)
+
+
+num1 = 3
+num2 = 14
+
+multiply_numbers = (num1 * num2)
+print(multiply_numbers)
