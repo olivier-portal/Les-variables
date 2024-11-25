268fb8792774a68b8ff858e9c309436b5f7cdfa6
OLIVIER PORTAL <olivier.portal@laplateforme.io>
2024-11-25 18:31:43 +0100

    add 10% increase on price

--- inventory.py
+++ inventory.py
@@ -1,14 +1,16 @@
+# Define article
 product_detail = {
     "article": "chemise",
     "price": 65,
     "quantity": 52,
 }
 
+# Show details of the article and ask if user want to buy some - modify quantity if user buy
 print(f'Informations du produit ====> Nom : {product_detail.get("article")}, prix : {product_detail.get("price")}€, Quantité : {product_detail.get("quantity")}, "voulez-vous en achetez ?", "O/N"')
 
 user_choice = input()
 
-if user_choice == "O":
+if user_choice == "O" or user_choice == "o":
     print("Combien voulez-vous en acheter ?")
     number_order = int(input())
     if number_order <= product_detail["quantity"]:
@@ -16,6 +18,12 @@ if user_choice == "O":
         print(f"Quantité restante : {product_detail["quantity"]}")
     else:
         print("Désolé nous n'avons pas assez de stock !")  
-else:
+elif user_choice == "N" or user_choice == "n":
     print("Dommage ! Peut-être une autre fois !")
-
+else:
+    print("Il faut répondre O pour oui ou N pour non")
+    
+# Increase price by 10%
+product_detail["price"] = 65 * 1.1
+product_detail
+print(f'Informations du produit ====> Nom : {product_detail.get("article")}, prix : {product_detail.get("price")}€, Quantité : {product_detail.get("quantity")}')
