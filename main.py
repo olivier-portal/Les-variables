1c2066774436c15816813aba867e6d77337270a7
OLIVIER PORTAL <olivier.portal@laplateforme.io>
2024-11-25 10:51:17 +0100

    Job01 => Job06

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 print(10+ 3)
 print(10 - 3)
+import string
+lower = list(string.ascii_lowercase)
+print(lower)
+print(lower[::-1])
+
+phrase = 'Je suis un String'
+ma_string: string = phrase
+print(ma_string)
+
 
